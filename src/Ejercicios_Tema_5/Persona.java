7b1e274a84cc2dc7a12124de295f6aaece3efb2d
Abel <abelboy94@gmail.com>
2022-08-02 20:07:21 +0200

    Ejercicios del tema 5 realizados.

---
+++ src/Ejercicios_Tema_5/Persona.java
@@ -0,0 +1,51 @@
+package Ejercicios_Tema_5;
+
+public class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+
+    public int getEdad(){
+        return this.edad;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+
+    }
+
+    public String getTelefono(){
+        return this.telefono;
+    }
+
+
+    public static void main(String[] args){
+
+        Persona fernando = new Persona();
+        fernando.setEdad(25);
+        fernando.setNombre("Fernando");
+        fernando.setTelefono("+34 651 25 22 11");
+
+        System.out.println(fernando.getNombre() + " esta en el sistema.\n" +
+                            "Edad: " + fernando.getEdad() + "\n" +
+                            "Teléfono: "+ fernando.getTelefono());
+
+
+    }
+
+
+
+
+}
