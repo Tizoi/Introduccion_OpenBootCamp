8c88efbb503e4724c653c353a128327c3157f3c0
Abel <abelboy94@gmail.com>
2022-08-02 13:07:27 +0200

    Primer ejercicio de OpenBootCamp

---
+++ src/Coche.java
@@ -0,0 +1,11 @@
+public class Coche {
+    int num_Puertas;
+
+    public Coche(){
+        int num_Puertas = 0;
+    }
+
+    public void poner_Puerta(){
+        num_Puertas++;
+    }
+}
---
+++ src/Ejercicio_1.java
@@ -0,0 +1,18 @@
+public class Ejercicio_1 {
+
+    public static int suma_TRES(int a, int b, int c){
+        return a+b+c;
+    }
+    public static void main(String[] args){
+        System.out.println("El resultado es: "+Ejercicio_1.suma_TRES(1,2,3));
+
+        Coche coche_Ejercicio = new Coche();
+        coche_Ejercicio.poner_Puerta();
+        coche_Ejercicio.poner_Puerta();
+        coche_Ejercicio.poner_Puerta();
+        coche_Ejercicio.poner_Puerta();
+
+        System.out.println("El número de puertas del coche es: "+ coche_Ejercicio.num_Puertas);
+    }
+}
+
